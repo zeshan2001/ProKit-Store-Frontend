0af5149281180359598378d0f72d03650527bb4d
zeshan2001 <zeshanbh@gmail.com>
2025-11-02 18:16:02 +0300

    created vite

--- README.md
+++
@@ -1,1 +0,0 @@
-# ProKit-Store-Frontend
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>Header</header>
+      <main>Main</main>
+    </>
+  )
+}
+
+export default App
