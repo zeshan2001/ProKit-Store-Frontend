26a2787432912db4af1015e5df4ce5ca80859c53
zeshan2001 <zeshanbh@gmail.com>
2025-11-03 21:53:25 +0300

    handling data in sign in page is done

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const SignIn = () => {
+  const initialState = {
+    email: '',
+    password: '',
+  }
+  const [formValues, setFormValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormValues({...formValues, [e.target.name]: e.target.value})
+  }
+
+  let navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formValues)
+    setFormValues(initialState)
+    navigate('/')
+
+  }
+
   return (
     <div>
-      <form >
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email </label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="password">Password </label>
-          <input type="password" name="password" id="password" />
+          <input type="password" name="password" id="password" onChange={handleChange} />
         </div>
         <button>Log In</button>
       </form>
