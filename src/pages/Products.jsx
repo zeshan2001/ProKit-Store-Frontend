f5f53815e218abff5776880c69fd531a41d823b0
zeshan2001 <zeshanbh@gmail.com>
2025-11-07 20:39:00 +0300

    search feature added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,25 +5,61 @@ import Product from "../components/Product"
 const Products = () => {
 
   const [products, setProducts] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [searched, toggleSearched] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
 
   useEffect(() => {
     const getProducts = async () => {
       const data = await GetAllProducts()
       setProducts(data)
     }
-
     getProducts()
   }, [])
+  
+  const filterProductsByName = (products, keyword) => {
+    return products.filter(product => product.name.toLowerCase().includes(keyword.toLowerCase()) )
+  }
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value)
+  }
+
+  const getSearchResults = async (e) => {
+    e.preventDefault()
+    searchQuery && setSearchResults(filterProductsByName(products, searchQuery))
+    searchQuery && toggleSearched(!searched)
+    setSearchQuery('')
+  }
+
   return (
     products.length ? (
       <div>
-        <h2>List of Products</h2>
-        <hr />
         <div>
-          {products.map(
-              (product) => <Product key={product._id} product = {product}/>
-              )}
+          <input type="text" name="searcg" id="search" onChange={handleChange} value={searchQuery}/>
+          <button onClick={getSearchResults}>Search</button>
         </div>
+        {
+          searched ? (
+          <div>
+            <p>Results: </p>
+            {
+              searchResults.map(
+                (product) => <Product key={product._id} product = {product}/>
+              )
+            }
+          </div> ) : (
+            <div>
+              <h2>List of Products</h2>
+              <hr />
+              <div>
+                {products.map(
+                    (product) => <Product key={product._id} product = {product}/>
+                    )}
+              </div>
+            </div>
+          )
+        }
       </div>
     ) : (
       <div>
